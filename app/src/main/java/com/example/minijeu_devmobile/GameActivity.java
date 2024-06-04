d6bd0c3fb3e21bd5045becef3c27e9c6d79066b9
Matthys Verbregue <matthys.verbregue@gmail.com>
2024-06-04 18:25:33 +0200

    Création du projet, ajout de la page activity_main.xml et redirection vers activity_game.xml

--- app/src/main/java/com/example/minijeu_devmobile/GameActivity.java
+++ app/src/main/java/com/example/minijeu_devmobile/GameActivity.java
@@ -2,7 +2,6 @@ package com.example.minijeu_devmobile;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -11,21 +10,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button boutonMain;
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        boutonMain = findViewById(R.id.buttonToGame);
-        boutonMain.setOnClickListener(view -> {
-            Intent intent = new Intent(this,GameActivity.class);
-            startActivity(intent);
-            Toast.makeText(this,"Amusez vous bien ;)",Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_game);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
     }
 }
